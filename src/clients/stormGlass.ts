d771ff538aefa2eb9420cd6fb48af77c24f3cafd
João Bispo <vitor.vi10@hotmail.com>
2021-03-14 12:58:55 -0300

    chore: added forecast mocks & forecast fixtures

---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,13 @@
+import { AxiosStatic } from 'axios';
+
+export class StormGlass {
+  readonly stormGlassAPIParams =
+    'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+  readonly stormGlassAPISource = 'noaa';
+  constructor(protected request: AxiosStatic) {}
+  public async fetchPoints(lat: number, lng: number): Promise<{}> {
+    return this.request.get(
+      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=,4%7Blat%7D&lng=,4%7Blng%7D`
+    );
+  }
+}
