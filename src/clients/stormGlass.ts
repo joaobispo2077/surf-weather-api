ce02c9b5acb1fb1dda9cf5744fdf6fd421588f09
João Bispo <vitor.vi10@hotmail.com>
2021-03-16 11:40:10 -0300

    test: added mocked axios

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -37,7 +37,12 @@ export class StormGlass {
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     const response = await this.request.get<StormGlassForecastResponse>(
-      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=,4%7Blat%7D&lng=,4%7Blng%7D`
+      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=,4%7Blat%7D&lng=,4%7Blng%7D`,
+      {
+        headers: {
+          Authorization: 'fake-token',
+        },
+      }
     );
     return this.normalizeResponse(response.data);
   }
