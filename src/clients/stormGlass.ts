279215424c9dd712def64460331649d4c11b216c
João Bispo <vitor.vi10@hotmail.com>
2021-03-17 10:57:35 -0300

    test: added should get a StormGlassResponseError when the StormGlas service responds with error

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -36,6 +36,15 @@ export class ClientRequestError extends InternalError {
     super(`${internalMessage}: ${message}`);
   }
 }
+
+export class StormGlassResponseError extends InternalError {
+  constructor(message: string) {
+    const internalMessage =
+      'Unexpected error returned by the StormGlass service';
+    super(`${internalMessage}: ${message}`);
+  }
+}
+
 export class StormGlass {
   readonly stormGlassAPIParams =
     'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
@@ -55,6 +64,12 @@ export class StormGlass {
       );
       return this.normalizeResponse(response.data);
     } catch (err) {
+      if (err.response && err.response.status)
+        throw new StormGlassResponseError(
+          `Error: ${JSON.stringify(err.response.data)} Code: ${
+            err.response.status
+          }`
+        );
       throw new ClientRequestError(err.message);
     }
   }
