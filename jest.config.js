ddd692b861502d721280a450ecdce9ac6d68c62d
João Bispo <joaobispo2077@gmail.com>
2021-06-19 12:51:39 -0300

    build: add jest, ts-jest and config unit test and e2e test

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { resolve } = require("path");
+const root = resolve(__dirname);
+
+module.exports = {
+  rootDir: root,
+  displayName: "root-tests",
+  testMatch: ["<rootDir>/src/**/*.ts"],
+  testEnvironment: "node",
+  clearMocks: true,
+  preset: "ts-jest",
+  moduleNameMapper: {
+    "@src/(.*)": "<rootDir>/src/$1",
+    "@test/(.*)": "<rootDir>/test/$1",
+  },
+};
