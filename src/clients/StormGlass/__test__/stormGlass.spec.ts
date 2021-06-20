cc4a55b63e88b05bb8dfab6bf90a660620b1e2bb
João Bispo <joaobispo2077@gmail.com>
2021-06-20 15:37:05 -0300

    feat: add axios mock to simulate stormglass response

--- src/clients/StormGlass/__test__/stormGlass.spec.ts
+++ src/clients/StormGlass/__test__/stormGlass.spec.ts
@@ -1,12 +1,20 @@
+import axios from 'axios';
 import { StormGlass } from '@src/clients/StormGlass';
+
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+jest.mock('axios');
 
 describe('StormGlass Client', () => {
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.7927554;
     const lng = 151.232545;
 
-    const stormGlass = new StormGlass();
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
+
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({});
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
 });
