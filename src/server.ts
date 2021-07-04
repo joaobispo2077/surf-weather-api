8513d8ccaa8c95fdc91dfa9eaa9fe5d16a983b1e
João Bispo <joaobispo2077@gmail.com>
2021-07-04 18:02:51 -0300

    feat: add beaches controller

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { Server } from '@overnightjs/core';
 import '@src/util/module-alias';
 import { ForecastController } from './controllers/forecast';
 import * as database from '@src/database';
+import { BeachesController } from './controllers/beaches';
 
 export class SetupServer extends Server {
 	constructor(private port = 3000) {
@@ -27,7 +28,8 @@ export class SetupServer extends Server {
 
 	private setupControllers(): void {
 		const forecastController = new ForecastController();
-		this.addControllers([forecastController]);
+		const beachesController = new BeachesController();
+		this.addControllers([forecastController, beachesController]);
 	}
 
 	private async setupDatabase(): Promise<void> {
