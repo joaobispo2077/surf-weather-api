7f4ae39f3cbbbdd681ce93075f2a0423056f241f
João Bispo <joaobispo2077@gmail.com>
2021-07-04 18:21:41 -0300

    test: should create a beach with success

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,13 @@
 import { Controller, Post } from '@overnightjs/core';
+import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
 
 @Controller('beaches')
 export class BeachesController {
 	@Post('')
 	public async create(req: Request, res: Response): Promise<void> {
-		res.status(201).send(Object.assign({ id: 'fake-id' }, req.body));
+		const beach = new Beach(req.body);
+		const result = await beach.save();
+		res.status(201).send(result);
 	}
 }
