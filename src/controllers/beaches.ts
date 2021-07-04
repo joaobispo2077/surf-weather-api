76adb0e6158e87c9af86a28e08752960512e48c4
João Bispo <joaobispo2077@gmail.com>
2021-07-04 18:30:31 -0300

    test: should throw 422 when there is a validation error

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,13 +1,23 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
 	@Post('')
 	public async create(req: Request, res: Response): Promise<void> {
-		const beach = new Beach(req.body);
-		const result = await beach.save();
-		res.status(201).send(result);
+		try {
+			const beach = new Beach(req.body);
+			const result = await beach.save();
+			res.status(201).send(result);
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		} catch (err: any) {
+			if (err instanceof mongoose.Error.ValidationError) {
+				res.status(422).send({ error: err.message });
+			} else {
+				res.status(500).send({ error: 'Internal Server Error' });
+			}
+		}
 	}
 }
