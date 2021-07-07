b6ed8ab2e7bdf65b38685662d3514a53f8cc772b
João Bispo <joaobispo2077@gmail.com>
2021-07-07 20:32:20 -0300

    test: should return 400 when there is a validation error

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -6,8 +6,12 @@ import { Request, Response } from 'express';
 export class UsersController {
 	@Post('')
 	public async create(req: Request, res: Response): Promise<void> {
-		const user = new User(req.body);
-		const newUser = await user.save();
-		res.status(201).send(newUser);
+		try {
+			const user = new User(req.body);
+			const newUser = await user.save();
+			res.status(201).send(newUser);
+		} catch (err: any) {
+			res.status(400).send({ error: err.message });
+		}
 	}
 }
