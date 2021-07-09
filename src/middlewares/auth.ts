044d1d7fd6b3238a78f250ccb6de20b521027952
João Bispo <joaobispo2077@gmail.com>
2021-07-09 12:49:03 -0300

    feat: return UNAUTHORIZED if there is a problem on the token verification

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,13 +1,20 @@
 import AuthService from '@src/services/auth';
 import { NextFunction, Request, Response } from 'express';
+import { JsonWebTokenError } from 'jsonwebtoken';
 
 export function authMiddleware(
 	req: Partial<Request>,
-	_: Partial<Response>,
+	res: Partial<Response>,
 	next: NextFunction,
 ): void {
-	const token = req.headers?.['x-access-token'];
-	const decodedToken = AuthService.decodeToken(token as string);
-	req.decodedToken = decodedToken;
-	next();
+	try {
+		const token = req.headers?.['x-access-token'];
+		const decodedToken = AuthService.decodeToken(token as string);
+		req.decodedToken = decodedToken;
+		next();
+	} catch (err) {
+		if (err instanceof JsonWebTokenError) {
+			res.status?.(401).send({ code: 401, error: err.message });
+		}
+	}
 }
