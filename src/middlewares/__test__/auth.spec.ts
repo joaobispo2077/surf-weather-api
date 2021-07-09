dd2734afa672ea08c7e3ea9b7cf23560d0ea5014
João Bispo <joaobispo2077@gmail.com>
2021-07-09 12:48:49 -0300

    test: should return UNAUTHORIZED if there is a problem on the token verification

--- src/middlewares/__test__/auth.spec.ts
+++ src/middlewares/__test__/auth.spec.ts
@@ -19,4 +19,30 @@ describe('Auth middleware', () => {
 
 		expect(nextFake).toHaveBeenCalled();
 	});
+
+	it('should return UNAUTHORIZED if there is a problem on the token verification', () => {
+		const requestFake = {
+			headers: {
+				'x-access-token': 'invalid token',
+			},
+		};
+
+		const sendMock = jest.fn();
+		const responseFake = {
+			status: jest.fn(() => ({
+				send: sendMock,
+			})),
+		};
+
+		const nextFake = jest.fn();
+
+		// eslint-disable-next-line @typescript-eslint/ban-types
+		authMiddleware(requestFake, responseFake as object, nextFake);
+
+		expect(responseFake.status).toHaveBeenCalledWith(401);
+		expect(sendMock).toHaveBeenCalledWith({
+			code: 401,
+			error: 'jwt malformed',
+		});
+	});
 });
