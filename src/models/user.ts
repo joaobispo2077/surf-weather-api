ae7b385ad4b622196a41b02736a7d393abe5c48f
João Bispo <joaobispo2077@gmail.com>
2021-07-09 10:37:08 -0300

    refactor: add auth service

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,5 @@
+import AuthService from '@src/services/auth';
 import mongoose, { Model, Document } from 'mongoose';
-import bcrypt from 'bcrypt';
 
 export enum CUSTOM_VALIDATION {
 	DUPLICATED = 'DUPLICATED',
@@ -44,27 +44,13 @@ schema.path('email').validate(
 	CUSTOM_VALIDATION.DUPLICATED,
 );
 
-export async function hashPassword(
-	password: string,
-	salt = 10,
-): Promise<string> {
-	return await bcrypt.hash(password, salt);
-}
-
-export async function comparePasswords(
-	password: string,
-	hashPassword: string,
-): Promise<boolean> {
-	return await bcrypt.compare(password, hashPassword);
-}
-
 schema.pre<UserModel>('save', async function (): Promise<void> {
 	if (!this.password || !this.isModified('password')) {
 		return;
 	}
 
 	try {
-		const hashedPassword = await hashPassword(this.password);
+		const hashedPassword = await AuthService.hashPassword(this.password);
 		this.password = hashedPassword;
 	} catch (err) {
 		console.error(`Error hashing the password for the user ${this.email}`, err);
