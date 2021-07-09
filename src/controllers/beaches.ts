52ce0809406224b0a417ff67af0536b6cb0b4a4a
João Bispo <joaobispo2077@gmail.com>
2021-07-09 13:10:28 -0300

    feat: merge user idwith beach payload to create a beach

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,14 +1,18 @@
-import { Controller, Post } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Post } from '@overnightjs/core';
+import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beach';
 import { Request, Response } from 'express';
 import mongoose from 'mongoose';
 
 @Controller('beaches')
+@ClassMiddleware(authMiddleware)
 export class BeachesController {
 	@Post('')
 	public async create(req: Request, res: Response): Promise<void> {
 		try {
-			const beach = new Beach(req.body);
+			const beach = new Beach(
+				Object.assign({}, req.body, { user: req.decodedToken?.id }),
+			);
 			const result = await beach.save();
 			res.status(201).send(result);
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
