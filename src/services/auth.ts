9bc4ca977d7e179d283301174e1ca1bf83086e24
João Bispo <joaobispo2077@gmail.com>
2021-07-09 12:28:54 -0300

    feat: add decodeToken into AuthService

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,6 +1,11 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import config from 'config';
+import { User } from '@src/models/user';
+
+export interface DecodedUser extends Omit<User, '_id'> {
+	id: string;
+}
 export default class AuthService {
 	public static async hashPassword(
 		password: string,
@@ -21,5 +26,10 @@ export default class AuthService {
 		return jwt.sign(payload, config.get('App.auth.key'), {
 			expiresIn: config.get('App.auth.tokenExpiresIn'),
 		});
+	}
+
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	public static decodeToken(token: string): DecodedUser {
+		return jwt.verify(token, config.get('App.auth.key')) as DecodedUser;
 	}
 }
