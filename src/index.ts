d98cfdcfaa56f308c988b29f61678a1b607996ef
João Bispo <joaobispo2077@gmail.com>
2021-07-10 10:59:38 -0300

    feat: add deployment settings into app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { SetupServer } from './server';
 import config from 'config';
 
+const port = process.env.PORT || config.get('App.port') || 3000;
+
 (async (): Promise<void> => {
-	const server = new SetupServer(config.get('App.port'));
+	const server = new SetupServer(port as number);
 	await server.init();
 	server.start();
 })();
