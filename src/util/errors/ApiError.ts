d0b685b9ee4335b17237ac68539255fd60c2912e
João Bispo <joaobispo2077@gmail.com>
2021-07-14 09:46:22 -0300

    feat: add ApiError class to format errors

---
+++ src/util/errors/ApiError.ts
@@ -0,0 +1,28 @@
+import httpStatusCode from 'http-status-codes';
+
+export interface APIError {
+	message: string;
+	code: number;
+	codeAsString?: string;
+	description?: string;
+	documentation?: string;
+}
+
+export interface APIErrorResponse extends Omit<APIError, 'codeAsString'> {
+	error: string;
+}
+
+export default class ApiError {
+	public static format(error: APIError): APIErrorResponse {
+		return Object.assign(
+			{},
+			{
+				message: error.message,
+				code: error.code,
+				error: error.codeAsString || httpStatusCode.getStatusText(error.code),
+			},
+			error.documentation && { documentation: error.documentation },
+			error.description && { description: error.description },
+		);
+	}
+}
