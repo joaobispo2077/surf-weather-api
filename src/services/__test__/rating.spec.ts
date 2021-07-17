d7bafb3b92e51d06cfd3bc2938119c8d703117d1
João Bispo <joaobispo2077@gmail.com>
2021-07-17 10:59:31 -0300

    test: ensure rating by swell period

--- src/services/__test__/rating.spec.ts
+++ src/services/__test__/rating.spec.ts
@@ -46,4 +46,26 @@ describe('Rating service', () => {
 			expect(rating).toBe(5);
 		});
 	});
+
+	describe('Get rating based on swell period', () => {
+		it('should get a rating of 1 for period of 5 seconds', () => {
+			const rating = defaultRating.getRatingForSwellPeriod(5);
+			expect(rating).toBe(1);
+		});
+
+		it('should get a rating of 2 for period of 9 seconds', () => {
+			const rating = defaultRating.getRatingForSwellPeriod(9);
+			expect(rating).toBe(2);
+		});
+
+		it('should get a rating of 4 for period of 12 seconds', () => {
+			const rating = defaultRating.getRatingForSwellPeriod(12);
+			expect(rating).toBe(4);
+		});
+
+		it('should get a rating of 5 for period of 16 seconds', () => {
+			const rating = defaultRating.getRatingForSwellPeriod(16);
+			expect(rating).toBe(5);
+		});
+	});
 });
