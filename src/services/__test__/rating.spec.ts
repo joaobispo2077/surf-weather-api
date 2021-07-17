1a23417de27a89a289b2728a21c5d3311a3aca2d
João Bispo <joaobispo2077@gmail.com>
2021-07-17 11:18:33 -0300

    test: ensure calculation of rating by swell height

--- src/services/__test__/rating.spec.ts
+++ src/services/__test__/rating.spec.ts
@@ -47,6 +47,10 @@ describe('Rating service', () => {
 		});
 	});
 
+	/**
+	 * Period calculation only tests
+	 */
+
 	describe('Get rating based on swell period', () => {
 		it('should get a rating of 1 for period of 5 seconds', () => {
 			const rating = defaultRating.getRatingForSwellPeriod(5);
@@ -65,6 +69,32 @@ describe('Rating service', () => {
 
 		it('should get a rating of 5 for period of 16 seconds', () => {
 			const rating = defaultRating.getRatingForSwellPeriod(16);
+			expect(rating).toBe(5);
+		});
+	});
+
+	/**
+	 *	Swell height specific logic calculation
+	 */
+
+	describe('Get rating based on swell height', () => {
+		it('should get rating 1 for less than ankle to knee high swell', () => {
+			const rating = defaultRating.getRatingForSwellSize(0.2);
+			expect(rating).toBe(1);
+		});
+
+		it('should get rating 2 for an ankle to knee swell', () => {
+			const rating = defaultRating.getRatingForSwellSize(0.6);
+			expect(rating).toBe(2);
+		});
+
+		it('should get rating 3 for waist high swell', () => {
+			const rating = defaultRating.getRatingForSwellSize(1.5);
+			expect(rating).toBe(3);
+		});
+
+		it('should get rating 5 for overhead swell', () => {
+			const rating = defaultRating.getRatingForSwellSize(2.5);
 			expect(rating).toBe(5);
 		});
 	});
