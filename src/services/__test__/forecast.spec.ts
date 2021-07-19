a80e357973bc12561788fe9173354b22ff6461d9
João Bispo <joaobispo2077@gmail.com>
2021-07-19 10:48:08 -0300

    test: ensure return forecast ordering by good rating

--- src/services/__test__/forecast.spec.ts
+++ src/services/__test__/forecast.spec.ts
@@ -17,7 +17,7 @@ describe('Forecast Service', () => {
 		},
 	];
 
-	it('should return the forecast for mutiple beaches in the same hour with different ratings', async () => {
+	it('should return the forecast for mutiple beaches in the same hour with different ratings ordered by rating', async () => {
 		mockedStormGlassService.fetchPoints.mockResolvedValueOnce([
 			{
 				swellDirection: 123.41,
@@ -67,21 +67,6 @@ describe('Forecast Service', () => {
 				forecast: [
 					{
 						lat: -33.792726,
-						lng: 151.289824,
-						name: 'Manly',
-						position: 'E',
-						rating: 2,
-						swellDirection: 123.41,
-						swellHeight: 0.21,
-						swellPeriod: 3.67,
-						time: '2020-04-26T00:00:00+00:00',
-						waveDirection: 232.12,
-						waveHeight: 0.46,
-						windDirection: 310.48,
-						windSpeed: 100,
-					},
-					{
-						lat: -33.792726,
 						lng: 141.289824,
 						name: 'Dee Why',
 						position: 'S',
@@ -93,6 +78,21 @@ describe('Forecast Service', () => {
 						waveDirection: 231.38,
 						waveHeight: 2.07,
 						windDirection: 299.45,
+						windSpeed: 100,
+					},
+					{
+						lat: -33.792726,
+						lng: 151.289824,
+						name: 'Manly',
+						position: 'E',
+						rating: 2,
+						swellDirection: 123.41,
+						swellHeight: 0.21,
+						swellPeriod: 3.67,
+						time: '2020-04-26T00:00:00+00:00',
+						waveDirection: 232.12,
+						waveHeight: 0.46,
+						windDirection: 310.48,
 						windSpeed: 100,
 					},
 				],
